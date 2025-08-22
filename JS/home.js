21651640fa307cdc60fd42581d20088db74d0db5
adnan123456a <abdullahaladnan95@gmail.com>
2025-08-22 04:54:54 -0400

    some styles fixed

--- JS/home.js
+++ JS/home.js
@@ -1,11 +1,5 @@
 const profileButton = document.querySelector(".profile-icon");
 
 profileButton.addEventListener('click',()=>{
-    if(profileButton.dataset.account === "guest") {
-        window.location.href = "/Login"
-        // I will add a modal later ! 
-    }
-    else {
-        window.location.href = `/Profile/${profileButton.dataset.account}`;
-    }
+    window.location.href = `/Profile`
 })
