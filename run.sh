3dfc7b493f8c9295f18e6b93d2680699f8f82577
Abdullah Al Adnan <130243149+adnan123456a@users.noreply.github.com>
2025-09-07 11:47:44 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-npm run dev
+echo "Running AniPub Dev. Mode"
+echo "Please Let Me Know If you find any Bug ....."
+echo "Starting Server....."
+
+if [ -v ~/usr/bin/npm ] 
+then 
+    npm run dev
+else 
+    if [ -f ~/etc/debian_version ]
+    then 
+        sudo apt update && upgrade -y
+        sudo apt install npm
+    else 
+        sudo pacman -Syu -y
+        sudo pacman -S npm
+
+    fi
+fi    
+        
