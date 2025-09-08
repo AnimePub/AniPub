462322e400b6c2977025d1bf65eeb7f67993fa2e
Abdullah Al Adnan <130243149+adnan123456a@users.noreply.github.com>
2025-09-08 14:48:09 -0400

    Update home.js

--- backend/router/home.js
+++ backend/router/home.js
@@ -14,7 +14,7 @@ HomeRouter.get("/Home", async (req, res) => {
     const animeDb = await AnimeDB.find().sort({
         updatedAt: -1
     }).limit(20);
-    const DBarray = [11, 10, 6, 9,16,17]
+    const DBarray = [9,10,13,6]
     const DBAnime = await AnimeDB.find({
         _id: {
             $in: DBarray
