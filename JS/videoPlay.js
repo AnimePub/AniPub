beacc4784e800fee5d90ede3df99123efebb2ac7
adnan123456a <abdullahaladnan95@gmail.com>
2025-09-09 14:03:05 -0400

    video player removed

--- JS/videoPlay.js
+++
@@ -1,7 +0,0 @@
-const PlayingVideo = document.querySelector(".Play-button");
-const play = document.querySelector("video");
-
-PlayingVideo.addEventListener("click", () => {
-    play.play();
-    //hehe
-})
