67acc011656f5ed793a8f5d01af2a2e1912f19b2
adnan123456a <abdullahaladnan95@gmail.com>
2025-09-13 13:50:18 -0400

    ups :(

--- JS/home.js
+++ JS/home.js
@@ -45,7 +45,7 @@ const shower = (info) =>{
     else {
       let fl = JSON.parse(info);
       MSGEBOX = `
-       <a style="text-decoration:none; width:100%" href="/AniPlayer/${fl.Id}/0"> <div class="fdivS" data-anime="${fl.Id}">
+       <a href="/AniPlayer/${fl.Id}/0" target="_blank"> <div class="fdivS" data-anime="${fl.Id}">
                 <img class="sdivImg" src="${fl.Image}" alt="">
                 <div>
                     <p class="sName">${fl.Name}</p>
