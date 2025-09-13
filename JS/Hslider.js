f8fd7b7fa214610b7578335ae7cd5dea39e5451a
adnan123456a <abdullahaladnan95@gmail.com>
2025-09-13 13:40:16 -0400

    home page is now responsive

--- JS/Hslider.js
+++ JS/Hslider.js
@@ -1,7 +1,7 @@
 const Slides = document.querySelectorAll(".slide");
 const EhName = document.querySelectorAll(".AniName");
 let init = 0;
-
+const names = document.querySelectorAll(".AniName")
 Slides.forEach((value, i) => {
     value.style.left = `${i*100}%`
 })
@@ -29,11 +29,59 @@ setInterval(() => {
     goleft()
 }, 5000);
 
-
-EhName.forEach(value => {
-    const ehe = value.innerText
-
-    if (ehe.length >= 30) {
-        value.style.fontSize = "30px"
+ names.forEach(value=>{
+        if(value.innerHTML.length >= 30) {
+              value.style.fontSize = "25px"
+        }
+        else {
+        value.style.fontSize = "40px"
     }
 })
+
+
+document.onchange = function (params) {
+    if (window.innerWidth < 830) {
+    
+    names.forEach(value=>{
+        if(value.innerHTML.length >= 30) {
+              value.style.fontSize = "15px"
+        }
+        else {
+        value.style.fontSize = "20px"
+    }
+    })
+}
+     if (window.innerWidth > 830) {
+        names.forEach(value=>{
+        if(value.innerHTML.length >= 30) {
+              value.style.fontSize = "25px"
+        }
+        else {
+        value.style.fontSize = "40px"
+    }
+    })
+     }
+
+}
+if (window.innerWidth < 830) {
+    names.forEach(value=>{
+        if(value.innerHTML.length >= 30) {
+              value.style.fontSize = "15px"
+        }
+        else {
+        value.style.fontSize = "20px"
+    }
+    })
+
+}
+
+if (window.innerWidth < 405) {
+     names.forEach(value=>{
+        if(value.innerHTML.length >= 30) {
+              value.style.fontSize = "15px"
+        }
+        else {
+        value.style.fontSize = "20px"
+    }
+})
+}
