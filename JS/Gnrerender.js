e22ca25068dae5dd8a52967a497c3a612dacb023
adnan123456a <abdullahaladnan95@gmail.com>
2025-09-14 11:39:15 -0400

    Genre Rende from SearchFeed

---
+++ JS/Gnrerender.js
@@ -0,0 +1,7 @@
+const GENREL = document.querySelectorAll(".genre");
+GENREL.forEach(value=>{
+    value.addEventListener('click',()=>{
+        const dataGen = value.dataset.genre ;
+        window.location.href=`Search?genre=${dataGen}`
+    })
+})
