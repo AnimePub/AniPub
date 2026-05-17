fbd250ade89ad652b4e2d6e432528b579692de9b
Abdullah Al Adnan <130243149+adnanisagoodboy@users.noreply.github.com>
2026-05-17 16:21:40 +0000

    genre seach fix

--- backend/router/searchGenre.js
+++ backend/router/searchGenre.js
@@ -35,7 +35,7 @@ SearchGenre.get("/Search",async(req,res)=>{
         alus = alus * 1
        }
      let linkI = `/account_circle_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg`;
-    AnimeDB.find({"Genres":query},{Name:1,ImagePath:1,DescripTion:1,_id:1,MALScore:1,RatingsNum:1,finder:1}).skip(alus).limit(20)
+    AnimeDB.find({"Genres":{$regex: query, $options: "i"}},{Name:1,ImagePath:1,DescripTion:1,_id:1,MALScore:1,RatingsNum:1,finder:1}).skip(alus).limit(20)
     .then(info=>{
         const AniData = info;
             if (Token) {
